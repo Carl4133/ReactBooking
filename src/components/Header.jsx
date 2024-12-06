f58ca580ef2d8ad2dbf3abe6ddd9cdbea4b3c7ae
Shao-Min Li <devil4133@gmail.com>
2024-12-06 08:52:37 +0800

    navbar complete

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import "./header.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCableCar,
+  faBed,
+  faPeopleGroup,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <h1 className="headerTitle">尋找下趟住宿</h1>
+        <p className="headerDes">
+          搜尋飯店、民宿及其他住宿類型的優惠...
+          <br />
+          Booking.com clone挑戰
+        </p>
+        <div className="headerSearchBar">
+          <div className="SearchBarItem">
+            <FontAwesomeIcon icon={faBed} />
+            <input
+              type="Search"
+              placeholder="你要去哪裡?"
+              className="SearchInput"
+            />
+          </div>
+          <div className="SearchBarItem">
+            <FontAwesomeIcon icon={faCableCar} />
+            <span className="SearchText">08/19/2022-08/16/2022</span>
+          </div>
+          <div className="SearchBarItem">
+            <FontAwesomeIcon icon={faPeopleGroup} />
+            <span className="SearchText">三位成人、2位小孩、1間房</span>
+          </div>
+          <buton className="SearchBarBtn">搜尋</buton>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Header from "../components/Header";
 
 import "./home.scss";
 
 //範例: const component = (變因variable) => return (結果Value) 輸出到網站上
+// <Navbar/ > 導入Navbar component
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <Header />
     </div>
   );
 };
