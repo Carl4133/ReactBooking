3dcd76e59964fff575e5fe1a6b954e71ca25bc5b
Shao-Min Li <devil4133@gmail.com>
2024-12-23 17:36:02 +0800

    date-fns 與 react-date-range 相容性問題要注意，可以選日期的版本

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,43 @@
-import React from "react";
-import "./header.scss";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faCableCar,
+  faCalendar,
   faBed,
   faPeopleGroup,
 } from "@fortawesome/free-solid-svg-icons"; //icon要注意
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useState } from "react";
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
+import { DateRange } from "react-date-range";
+
+import * as locales from "react-date-range/dist/locale"; //?
+import "./header.scss";
 
 const Header = () => {
+  //const [destination, setDestination] = useState(""); ???
+  //const [openConditions, setOpenConditions] = useState(false);
+  const [openCalendar, setOpenCalendar] = useState(false);
+
+  const [dates, setDates] = useState([
+    {
+      startDate: new Date(),
+      endDate: new Date(),
+      key: "selection",
+    },
+  ]);
+  /*   const [conditions, setConditions] = useState({
+    adult: 1, //初始人數,房間數為一
+    children: 0, //可以不一定要有小孩
+    room: 1,
+  });
+  const handleCounter = (name, sign) => {
+    setConditions((prev) => {
+      return {
+        ...prev,
+        [name]:
+          sign === "increase" ? conditions[name] + 1 : conditions[name] - 1,
+      };
+    });
+  }; */
   return (
     <div className="header">
       <div className="headerContainer">
@@ -27,8 +57,27 @@ const Header = () => {
             />
           </div>
           <div className="SearchBarItem">
-            <FontAwesomeIcon icon={faCableCar} />
-            <span className="SearchText">08/19/2022-08/16/2022</span>
+            <FontAwesomeIcon
+              icon={faCalendar}
+              onClick={() => setOpenCalendar(!openCalendar)}
+            />
+            <span
+              className="SearchText"
+              onClick={() => setOpenCalendar(!openCalendar)}
+            >
+              08/19/2022-08/16/2022
+            </span>
+            {openCalendar && (
+              <DateRange
+                editableDateInputs={true}
+                onChange={(item) => setDates([item.selection])}
+                moveRangeOnFirstSelection={false}
+                className="calendar"
+                ranges={dates}
+                minDate={new Date()}
+                locale={locales["zhTW"]}
+              />
+            )}
           </div>
           <div className="SearchBarItem">
             <FontAwesomeIcon icon={faPeopleGroup} />
